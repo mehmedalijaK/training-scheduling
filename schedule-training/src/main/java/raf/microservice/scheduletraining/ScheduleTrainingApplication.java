73daa0dc22e091400049d7e14b0d8b4519af53db
asakotic6821rn <asakotic6821rn@raf.rs>
2023-12-23 22:16:11 +0100

    init commit

---
+++ schedule-training/src/main/java/raf/microservice/scheduletraining/ScheduleTrainingApplication.java
@@ -0,0 +1,13 @@
+package raf.microservice.scheduletraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleTrainingApplication.class, args);
+	}
+
+}
